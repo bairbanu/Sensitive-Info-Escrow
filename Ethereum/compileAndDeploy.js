ac25d204d85e002f7a7f4141d5dcc2572b4048ad
Zubair Ahmed <zubair.ahmed@alumni.stanford.edu>
2018-05-10 21:09:55 -0700

    write deploy functionality for smart contracts

--- Ethereum/compileAndDeploy.js
+++ Ethereum/compileAndDeploy.js
@@ -2,22 +2,58 @@ const path = require('path')
 const solc = require('solc')
 const fs = require('fs-extra')
 
-const buildPath = path.resolve(__dirname, 'build')
-fs.removeSync(buildPath)
+const Web3 = require('web3')
+const web3 = new Web3('http://localhost:8545')
 
-const handOffContractPath = path.resolve(
-	__dirname,
-	'contracts',
-	'HandOff_SmartContracts.sol'
-)
-const contractSource = fs.readFileSync(
-	handOffContractPath,
-	'utf8'
-)
+function compile() {
+	const buildPath = path.resolve(__dirname, 'build')
+	fs.removeSync(buildPath)
 
-const contractOutput = solc.compile(contractSource, 1).contracts
+	const handOffContractPath = path.resolve(
+		__dirname,
+		'contracts',
+		'HandOff_SmartContracts.sol'
+	)
+	const contractSource = fs.readFileSync(
+		handOffContractPath,
+		'utf8'
+	)
 
-const receiverStorage = contractOutput[':ReceiverStorage']
-const senderStorage = contractOutput[':SenderStorage']
+	const contractOutput = solc.compile(contractSource, 1).contracts
 
-// deploy here
+	const receiverStorage = contractOutput[':ReceiverStorage']
+	const senderStorage = contractOutput[':SenderStorage']
+	return { receiverStorage, senderStorage }
+}
+
+/* 
+	Account 0 -- sender
+	Account 1 -- receiver
+ */
+
+async function deploy() {
+	const compiledContracts = compile()
+	const { receiverStorage, senderStorage } = compiledContracts
+
+	const { interface: receiverInterface, bytecode: receiverBytecode } = receiverStorage
+	const { interface: senderInterface, bytecode: senderBytecode } = senderStorage
+	
+	const accounts = await web3.eth.getAccounts()
+
+	const senderInstance = await new web3.eth.Contract(JSON.parse(senderInterface))
+		.deploy({ data: senderBytecode })
+		.send({ from: accounts[0], gas: '1000000' })
+
+	const receiverInstance = await new web3.eth.Contract(JSON.parse(receiverInterface))
+		.deploy({ data: receiverBytecode })
+		.send({ from: accounts[1], gas: '1000000'})
+
+	return {
+		receiverInterface,
+		receiverAddress: receiverInstance._address,
+		senderInterface,
+		senderAddress: senderInstance._address,
+	}	
+}
+
+export default deploy
