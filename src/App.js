2b21e33c69acda86d4b205152b624fac954200db
Zubair Ahmed <zubair.ahmed@alumni.stanford.edu>
2018-05-21 22:36:30 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -6,42 +6,54 @@ import web3 from './web3'
 class App extends Component {
 	state = {
 		receiverInstance: {},
-		senderInstance: {}
+		senderInstance: {},
+		senderValue: '',
+		receiverPubKey: '',
+		receiverPriKey: '',
 	}
 
 	async componentDidMount() {
-		const {
-			receiverABI,
-			receiverAddress,
-			senderABI,
-			senderAddress
-		} = contractJSON
+		const { receiverABI, receiverAddress, senderABI, senderAddress } = contractJSON
 
 		const parsedReceiverABI = JSON.parse(receiverABI)
 		const parsedSenderABI = JSON.parse(senderABI)
 
-		const receiverInstance = await new web3.eth.Contract(
-			parsedReceiverABI,
-			receiverAddress
-		)
-		const senderInstance = await new web3.eth.Contract(
-			parsedSenderABI,
-			senderAddress
+		const receiverInstance = await new web3.eth.Contract(parsedReceiverABI, receiverAddress)
+		const senderInstance = await new web3.eth.Contract(parsedSenderABI, senderAddress)
+
+		const pubKey = await web3.eth.getAccounts()
+		const receiverPubKey = pubKey[1]
+
+		const receiverPriKey = '691e1d3acbd5d18add987da48a552cb1dee44bc01d00d37fc8d7dd9c74e0932c'
+		this.setState({ receiverInstance, senderInstance, receiverPubKey })
+	}
+
+	/* 
+		Account 0 -- sender
+		Account 1 -- receiver
+	*/
+	handleSenderSubmit = async event => {
+		event.preventDefault()
+
+		const secret = this.state.senderValue
+		const receiverPubKey = this.state.receiverPubKey
+
+		const encrypted = await crypto.encryptWithPublicKey(
+			receiverPubKey,
+			secret
 		)
 
-		this.setState({
-			receiverInstance,
-			senderInstance
-		}, () => {
-			console.log(Object.keys(this.state.receiverInstance))
-		})
-
+		console.log('this is the encrypted message:', encrypted)
 	}
 
 	render() {
 		return (
 			<div>
-				<h1> Check console.log </h1>
+				<form onSubmit={ this.handleSenderSubmit }>
+					<label> Enter sender secret information: </label>
+					<input name="sender" value={ this.state.senderValue } onChange={ event => this.setState({ senderValue: event.target.value }) } />
+					<button> Send </button>
+				</form>
 			</div>
 		)
 	}
